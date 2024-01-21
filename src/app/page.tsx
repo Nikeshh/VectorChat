c1e7d20a6035bb4abbf16bf2aad30335a939da03
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-01-21 16:17:56 -0500

    S3 and Fileupload implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import FileUpload from "@/components/FileUpload";
 import { Button } from "@/components/ui/button";
 import { UserButton, auth } from "@clerk/nextjs";
+import { LogIn } from "lucide-react";
+import Link from "next/link";
 
 export default async function Home() {
 
@@ -17,9 +20,19 @@ export default async function Home() {
           <div className="flex mt-2">
             {isAuth && <Button>Go to Chats</Button>}
           </div>
-          <p>
-            Join millions of students, researchers
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, researchers and professionals to instantly answer questions and understand research with AI
           </p>
+          <div className="w-full mt-4">
+            {isAuth ? (<FileUpload />) : (
+              <Link href='/sign-in'>
+                  <Button>
+                    Login to get Started!
+                    <LogIn className="w-4 h-4 ml-2" />
+                  </Button>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </div>
